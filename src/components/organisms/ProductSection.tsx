8ebc2744c50bf1b9e7edf416c65caf0fceddb61e
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2023-01-20 23:23:39 -0300

    renderizando os produtos na página através de uma api, criando um input de seleção por menor e maior valor de produto

--- src/components/organisms/ProductSection.tsx
+++ src/components/organisms/ProductSection.tsx
@@ -1,12 +1,18 @@
 import Product from "../molecules/Product"
 import { DivProductSection } from "../../styled-components"
+import { iGetProductsResponse } from "../templates/Homepage"
 
-const ProductSection: React.FC = () => {
+const ProductSection: React.FC<{ productsList: iGetProductsResponse[], priceProduct: string }> = (props) => {
     return (
         <DivProductSection>
-            <Product src="https://http2.mlstatic.com/D_NQ_NP_906003-MLA51439581349_092022-W.jpg" p="Notebook" h4="R$3000" />
-            <Product src="https://www.havan.com.br/media/catalog/product/cache/73a52df140c4d19dbec2b6c485ea6a50/c/e/celular-smartphone-samsung-a53-5g-tela-6-1--8gb-ram-128gb_776212.jpg" p="Celular" h4="R$4000" />
-            <Product src="https://cdn.weasy.io/users/xiaomi/catalog/thumb_tv_p1e_43_1.png" p="Televisão" h4="R$2500" />
+            {props.productsList.sort((a, b) => {
+                if(props.priceProduct === "more"){
+                    return b.valor - a.valor   
+                }
+                return a.valor - b.valor
+            }).slice(0, 9).map((product) => {
+                return <Product imagem={product.imagem} nome={product.nome} valor={product.valor} marca={product.marca} categoria={product.categoria} />
+            })}
         </DivProductSection>
     )
 }
--- src/components/templates/Homepage.tsx
+++ src/components/templates/Homepage.tsx
@@ -1,13 +1,42 @@
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import InputSelect from "../atoms/InputSelect"
 import InformationSection from "../molecules/InformationsSection"
 import Header from "../organisms/Header"
 import ProductSection from "../organisms/ProductSection"
 
+export interface iGetProductsResponse{
+    nome: string,
+    marca: string,
+    valor: number,
+    categoria: string,
+    imagem: string,
+    id: number
+}
+
 const Homepage: React.FC = () => {
+    const[products, setProducts] = useState<iGetProductsResponse[]>([])
+    const[priceProduct, setPriceProduct] = useState<string>('less')
+
+    const fetchProducts: () => Promise<void> = async () => {
+        const response = await axios.get("https://apigenerator.dronahq.com/api/C9D5jR1z/mercadoLivre")
+        setProducts(response.data)
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    const handleClick = (evt: React.ChangeEvent<HTMLSelectElement>) => {
+        setPriceProduct(evt.currentTarget.value)
+    }
+
     return (
         <>
             <Header />
             <InformationSection />
-            <ProductSection />
+            <InputSelect onChange={handleClick} />
+            <ProductSection productsList={products} priceProduct={priceProduct} />
         </>
     )
 }
