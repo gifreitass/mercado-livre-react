6185e427332856c1ae4351ff056f7f3c2055b42d
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2023-01-26 00:11:26 -0300

    adicionando validações no formulário de cadastro do produto

--- src/components/templates/NewProduct.tsx
+++ src/components/templates/NewProduct.tsx
@@ -2,49 +2,72 @@ import { ButtonNewProduct, FormNewProduct, InputNewProduct, LabelNewProduct, Par
 import Header from "../organisms/Header"
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-
+import { useFormik } from "formik"
+import * as Yup from 'yup';
 
 const NewProduct: React.FC = () => {
-    const [formValues, setFormValues] = useState({ nome: "", marca: "", valor: "", categoria: "", imagem: "" })
     const [disabled, setDisabled] = useState(true)
 
+    const formik = useFormik({
+        initialValues: {
+            nome: "",
+            marca: "",
+            valor: "",
+            categoria: "",
+            imagem: ""
+        },
+        validationSchema:
+            Yup.object().shape({
+            nome: Yup.string()
+                .matches(/^[a-zA-Z]{0,}(?: [a-zA-Z]+){0,2}$/, 'O nome não pode conter caracteres especiais'),
+            valor: Yup.number()
+                .typeError('O campo deve ser preenchido com um número')
+                .integer('O número precisa ser inteiro')
+                .moreThan(0, 'O número precisa ser maior que zero'),
+            imagem: Yup.string()
+                .required('Campo obrigatório')
+        }),
+        onSubmit: async () => {
+            await axios.post("https://apigenerator.dronahq.com/api/C9D5jR1z/mercadoLivre", {
+                nome: formik.values.nome,
+                marca: formik.values.marca,
+                valor: Number(formik.values.valor),
+                categoria: formik.values.categoria,
+                imagem: formik.values.imagem
+            })
+            formik.resetForm()
+        }
+    })
+
     useEffect(() => {
-        setDisabled(!(!!formValues && !!formValues.nome && !!formValues.marca && !!formValues.valor && !!formValues.categoria && !!formValues.imagem))
-    }, [formValues])
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFormValues({ ...formValues, [event.target.id]: event.target.value })
-    }
-
-    const postProducts = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        axios.post("https://apigenerator.dronahq.com/api/C9D5jR1z/mercadoLivre", {
-            nome: formValues.nome,
-            marca: formValues.marca,
-            valor: Number(formValues.valor),
-            categoria: formValues.categoria,
-            imagem: formValues.imagem
-        })
-        setFormValues({ nome: "", marca: "", valor: "", categoria: "", imagem: "" })
-    }
+        setDisabled(!(
+            !!formik.values.nome &&
+            !!formik.values.marca &&
+            !!formik.values.valor &&
+            !!formik.values.categoria &&
+            !!formik.values.imagem))
+    }, [formik.values])
 
     return (
         <section>
             <Header />
             <div>
-                <FormNewProduct onSubmit={postProducts}>
+                <FormNewProduct onSubmit={formik.handleSubmit}>
                     <ParagraphNewProduct>Cadastre um novo produto abaixo:</ParagraphNewProduct>
                     <LabelNewProduct htmlFor="nome">Nome:</LabelNewProduct>
-                    <InputNewProduct type="text" id="nome" onChange={handleChange} value={formValues.nome} />
+                    <InputNewProduct type="text" id="nome" onChange={formik.handleChange} value={formik.values.nome} />
+                    {formik.errors.nome && (<div>{formik.errors.nome}</div>)}
                     <LabelNewProduct htmlFor="marca">Marca:</LabelNewProduct>
-                    <InputNewProduct type="text" id="marca" onChange={handleChange} value={formValues.marca} />
+                    <InputNewProduct type="text" id="marca" onChange={formik.handleChange} value={formik.values.marca} />
                     <LabelNewProduct htmlFor="valor">Valor:</LabelNewProduct>
-                    <InputNewProduct type="text" id="valor" onChange={handleChange} value={formValues.valor} />
+                    <InputNewProduct type="text" id="valor" onChange={formik.handleChange} value={formik.values.valor} />
+                    {formik.errors.valor && (<div>{formik.errors.valor}</div>)}
                     <LabelNewProduct htmlFor="categoria">Categoria:</LabelNewProduct>
-                    <InputNewProduct type="text" id="categoria" onChange={handleChange} value={formValues.categoria} />
+                    <InputNewProduct type="text" id="categoria" onChange={formik.handleChange} value={formik.values.categoria} />
                     <LabelNewProduct htmlFor="imagem">Imagem:</LabelNewProduct>
-                    <InputNewProduct type="text" id="imagem" onChange={handleChange} value={formValues.imagem} />
-                    <ButtonNewProduct type="submit" disabled={disabled}>Cadastrar</ButtonNewProduct>
+                    <InputNewProduct type="text" id="imagem" onChange={formik.handleChange} value={formik.values.imagem} />
+                    {formik.errors.imagem && (<div>{formik.errors.imagem}</div>)}
+                    <ButtonNewProduct type="submit" disabled={disabled} >Cadastrar</ButtonNewProduct>
                 </FormNewProduct>
             </div>
         </section>
